0e528359564cee94d026d6b355d8b688e8116b0d
jasenkoz <42840003+jasenkoz@users.noreply.github.com>
2018-08-31 20:55:57 +0200

    Comment added to ChineseZodiac

--- ChineseZodiac.java
+++ ChineseZodiac.java
@@ -1,3 +1,4 @@
+// Ispisivanje kineskog zodiak znaka na osnovu upisane godine
 import java.util.Scanner;
 
   public class ChineseZodiac {
@@ -8,18 +9,18 @@ import java.util.Scanner;
     int year = input.nextInt();
 
     switch (year % 12) {
-    case 0: System.out.println("monkey"); break;
-    case 1: System.out.println("rooster"); break;
-    case 2: System.out.println("dog"); break;
-    case 3: System.out.println("pig"); break;
-    case 4: System.out.println("rat"); break;
-    case 5: System.out.println("ox"); break;
-    case 6: System.out.println("tiger"); break;
-    case 7: System.out.println("rabbit"); break;
-    case 8: System.out.println("dragon"); break;
-    case 9: System.out.println("snake"); break;
-    case 10: System.out.println("horse"); break;
-    case 11: System.out.println("sheep");
+      case 0: System.out.println("monkey"); break;
+      case 1: System.out.println("rooster"); break;
+      case 2: System.out.println("dog"); break;
+      case 3: System.out.println("pig"); break;
+      case 4: System.out.println("rat"); break;
+      case 5: System.out.println("ox"); break;
+      case 6: System.out.println("tiger"); break;
+      case 7: System.out.println("rabbit"); break;
+      case 8: System.out.println("dragon"); break;
+      case 9: System.out.println("snake"); break;
+      case 10: System.out.println("horse"); break;
+      case 11: System.out.println("sheep");
     }
   }
 }
